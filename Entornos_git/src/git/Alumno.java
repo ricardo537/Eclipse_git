3ba25b148cb21e38bce6e6ade69fdda3a74a611f
ricardo537 <dawricardo735@gmail.com>
2023-04-28 11:39:52 +0200

    Punto 6

--- Entornos_git/src/git/Alumno.java
+++ Entornos_git/src/git/Alumno.java
@@ -4,4 +4,10 @@ public class Alumno {
 	private String nombre;
 	private String apellido;
 	private String dni;
+	
+	Alumno(String nombre, String apellido, String dni) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+	}
 }
