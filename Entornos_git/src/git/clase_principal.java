651fc8c068d54a303595ad3c87377da8e164a24b
ricardo537 <dawricardo735@gmail.com>
2023-04-28 12:12:50 +0200

    Punto 13

--- Entornos_git/src/git/clase_principal.java
+++ Entornos_git/src/git/clase_principal.java
@@ -4,13 +4,9 @@ public class clase_principal {
 
 	public static void main(String[] args) {
 		System.out.println("Listado de alumnos 1º DAW");
-<<<<<<< HEAD
-
-		Alumno uno = new Alumno("Federico", "Lopez", "79438395g");
+		Alumno uno = new Alumno();
 		System.out.println(uno.toString());
-=======
 		System.out.println("Alumno1:");
->>>>>>> refs/heads/Ricardo
 	}
 
 }
